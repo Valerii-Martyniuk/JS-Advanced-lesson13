84c6402dc662302b30b41fbcda8cdda4f5ee86d1
Valerii-Martyniuk <114944293+Valerii-Martyniuk@users.noreply.github.com>
2023-02-14 15:29:16 +0200

    Add files via upload

---
+++ src/app/components/call-back/call-back.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { MatDialog } from "@angular/material/dialog";
+
+@Component({
+  selector: "app-call-back",
+  templateUrl: "./call-back.component.html",
+  styleUrls: ["./call-back.component.scss"],
+})
+export class CallBackComponent implements OnInit {
+  public callBackForm!: FormGroup;
+
+  constructor(private fb: FormBuilder, private dialog: MatDialog) {}
+
+  ngOnInit(): void {
+    this.buildCallBackForm();
+  }
+
+  buildCallBackForm(): void {
+    this.callBackForm = this.fb.group({
+      name: [null, [Validators.required]],
+      phone: [null, [Validators.required]],
+    });
+  }
+
+  closeComponent(): void {
+    this.dialog.closeAll();
+  }
+
+  callBackReload(): void {
+    this.callBackForm.reset();
+    this.closeComponent();
+  }
+}
